e8ee3e5cdda969b735123ca153be5a65ea963fe9
Spring <Spring@DESKTOP-24LGQE4>
2020-08-04 23:20:07 +0900

    2020.08.04 내용 : 브루트 포스법 일곱 난장이, 퇴사 문제 해결

---
+++ src/brute_force/퇴사_14501.java
@@ -0,0 +1,39 @@
+package brute_force;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 퇴사_14501 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException{
+
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(bf.readLine());
+		int[] T = new int[N+2];
+		int[] P = new int[N+2];
+		int[] bp = new int[N+2];
+
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(bf.readLine());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
+		}
+		for (int i = N; i > 0; i--) {
+			int day = i+T[i];
+			
+			if(day <= N+1) {
+				bp[i] = Math.max(P[i]+bp[day], bp[i+1]);
+			}else {
+				bp[i] = bp[i+1];
+			}
+		}
+		
+		System.out.println(bp[1]);
+		
+
+	}
+}
---
+++ src/stack_queue/주식가격_programmers.java
@@ -0,0 +1,10 @@
+package stack_queue;
+
+public class 주식가격_programmers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
