e657102e790e4466a98a50a410959f818d53e109
ckadl0919 <ckadl0118@gmail.com>
2021-08-07 07:22:06 +0900

    update. 백준 민식어 update
    백준 민식어 update

--- src/백준/sort/민식어_1599.java
+++ src/백준/sort/민식어_1599.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class 민식어_1599 {
 
@@ -19,76 +18,74 @@ public class 민식어_1599 {
         Minsik m[] = new Minsik[n];
 
         for(i=0;i<n;i++) m[i] = new Minsik(in.readLine());
-        Arrays.sort(m, new Comparator<Minsik>(){
-            public int compare(Minsik a, Minsik b) {
-                return a.compareTo(b.str);
-            }
-        });
+        Arrays.sort(m, (a, b) -> a.compareTo(b.str));
 
         for(i=0;i<n;i++) res.append(m[i].str+"\n");
         out.write(res.toString());
         out.close();
         in.close();
     }
-}
 
-// 민식 클래스 구현
-class Minsik implements Comparable<String>{
+    // 민식 클래스 구현
+    static class Minsik implements Comparable<String>{
 
-    String str;
+        String str;
 
-    // 민식이 언어 인덱스
-    enum Alphabet{
-        a(0), b(1), k(2), d(3), e(4), g(5), h(6), i(7), l(8), m(9),
-        n(10), ng(11), o(12), p(13), r(14), s(15), t(16), u(17), w(18), y(19);
+        // 민식이 언어 인덱스
+        enum Alphabet{
+            a(0), b(1), k(2), d(3), e(4), g(5), h(6), i(7), l(8), m(9),
+            n(10), ng(11), o(12), p(13), r(14), s(15), t(16), u(17), w(18), y(19);
 
-        Alphabet(int num){ codeNum = num; }
+            Alphabet(int num){ codeNum = num; }
 
-        int codeNum;
+            int codeNum;
 
-        public int getCodeNum(){ return codeNum; }
-    }
-
-    public Minsik(String str){ this.str = str; }
-
-    // 단어를 민식이 언어 배열로 변환
-    private static String[] convert(String str){
-
-        int i, k = 0, n = str.length();
-
-        String ng = "ng", tmp;
-
-        String init[] = new String[n];
-
-        for(i = 0; i < n; i++){
-            // 기본적으로 두개씩 자르기
-            tmp = str.substring(i, i==n-1?i+1:i+2);
-
-            // ng면 ng 더하기, 아니면 앞자리만 더하기
-            if (tmp.equals(ng)) {
-                init[k++] = ng;
-                i++;
-            } else {
-                init[k++] = String.valueOf(tmp.charAt(0));
-            }
+            public int getCodeNum(){ return codeNum; }
         }
 
-        // null을 감안해 k만큼 잘라 return
-        return Arrays.copyOf(init, k);
-    }
-    public int compareTo(String anotherString) {
-        String []v1 = convert(str), v2 = convert(anotherString);
-        int k = 0, len1 = v1.length, len2 = v2.length, lim = Math.min(len1, len2);
+        public Minsik(String str){ this.str = str; }
 
-        // 작은 길이 만큼 반복
-        while (k < lim) {
-            String s1 = v1[k], s2 = v2[k];
-            if (!s1.equals(s2)) {
-                return Alphabet.valueOf(s1).getCodeNum() - Alphabet.valueOf(s2).getCodeNum();
+        // 단어를 민식이 언어 배열로 변환
+        private static String[] convert(String str){
+
+            int i, k = 0, n = str.length();
+
+            String ng = "ng", tmp;
+
+            String init[] = new String[n];
+
+            for(i = 0; i < n; i++){
+                // 기본적으로 두개씩 자르기
+                tmp = str.substring(i, i==n-1?i+1:i+2);
+
+                // ng면 ng 더하기, 아니면 앞자리만 더하기
+                if (tmp.equals(ng)) {
+                    init[k++] = ng;
+                    i++;
+                } else {
+                    init[k++] = String.valueOf(tmp.charAt(0));
+                }
             }
-            k++;
+
+            // null을 감안해 k만큼 잘라 return
+            return Arrays.copyOf(init, k);
         }
-        return len1 - len2;
+        public int compareTo(String anotherString) {
+            String []v1 = convert(str), v2 = convert(anotherString);
+            int k = 0, len1 = v1.length, len2 = v2.length, lim = Math.min(len1, len2);
+
+            // 작은 길이 만큼 반복
+            while (k < lim) {
+                String s1 = v1[k], s2 = v2[k];
+                if (!s1.equals(s2)) {
+                    return Alphabet.valueOf(s1).getCodeNum() - Alphabet.valueOf(s2).getCodeNum();
+                }
+                k++;
+            }
+            return len1 - len2;
+        }
     }
+
 }
+
 
