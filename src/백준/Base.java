4c676a17668a8d3be8bab955a0e72c6abfeea10a
ckadl0919 <ckadl0118@gmail.com>
2021-09-25 11:51:39 +0900

    feat. 백준 Base Code 추가
    백준 Base Code 추가

---
+++ src/백준/Base.java
@@ -0,0 +1,19 @@
+package 백준;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Base {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+
+
+}
+
+
